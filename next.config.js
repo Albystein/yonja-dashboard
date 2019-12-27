4655da8f162dba6996549bde13c5f7c1fe9f402b
Atem Aguer <atemsunday@gmail.com>
2019-12-26 22:55:33 -0500

    Yonja dashboard setup

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withLess = require('@zeit/next-less')
+const withCSS = require("@zeit/next-css")
+module.exports = withLess({
+  lessLoaderOptions: {
+  javascriptEnabled: true
+  },
+  withCSS()
+})
